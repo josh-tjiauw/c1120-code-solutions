f002430bb46e451191d4589605c85d8a8a177258
Josh Tjiauw <freezingmist2@gmail.com>
2020-11-23 10:54:53 -0800

    DOM Querying

---
+++ dom-querying/main.js
@@ -0,0 +1,18 @@
+console.log('hello, world');
+var h1Properties = document.querySelector('h1');
+console.log(h1Properties);
+console.dir(h1Properties);
+
+var explanationProperties = document.getElementById('explanation');
+console.log(explanationProperties);
+console.dir(explanationProperties);
+
+var hintProperties = document.getElementsByClassName('hint');
+console.log(hintProperties);
+console.dir(hintProperties);
+
+var pproperties = document.querySelectorAll('p');
+console.log(pproperties);
+
+var exampleLinkProperties = document.querySelectorAll('a.example-link');
+console.log(exampleLinkProperties);
