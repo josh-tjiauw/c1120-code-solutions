f739e31d37aefd46a57ef6371e8f69119cd4c375
Josh Tjiauw <freezingmist2@gmail.com>
2020-12-28 15:35:07 -0800

    Finished unique.js

--- array-manipulation-3/src/unique.js
+++ array-manipulation-3/src/unique.js
@@ -1,2 +1,10 @@
 /* exported unique */
-function unique(array){}
+function unique(array){
+  var unique = [];
+  for(var i = 0; i < array.length; i++){
+    if (!unique.includes(array[i])){
+      unique.push(array[i]);
+    }
+  }
+  return unique;
+}
