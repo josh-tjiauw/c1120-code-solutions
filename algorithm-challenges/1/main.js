932f06b72751f8d69615f6e4ad46085c4a5b670f
Josh Tjiauw <freezingmist2@gmail.com>
2020-12-29 11:26:47 -0800

    optimized algorithm.

--- algorithm-challenges/1/main.js
+++ algorithm-challenges/1/main.js
@@ -1,20 +1,3 @@
 function digital_root(n) {
-  nArray = [];
-  nToString = n.toString();
-  for(var i=0; i<nToString.length; i++){
-    nArray.push(+nToString.charAt(i));
-  }
-  for (var i = 0, sum = 0; i < nArray.length; i++){
-    sum+=nArray[i]
-    if (sum === 9) {
-      sum = 0;
-    }
-    else if (sum % 9 === 0) {
-      sum = 9;
-    }
-    else if (sum > 9){
-      sum-=9;
-    }
-  }
-  return sum;
+  return (n - 1) % 9 + 1;
 }
