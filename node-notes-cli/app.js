4a8eaee4cd6a866c8cd5a483ba3b08ce9f63b843
Josh Tjiauw <freezingmist2@gmail.com>
2021-01-05 15:29:14 -0800

    Finished Node Notes CLI Challenge

--- node-notes-cli/app.js
+++ node-notes-cli/app.js
@@ -12,12 +12,11 @@ const read = () => {
 const create = () => {
   fs.readFile('./data.json', 'utf8', (err, data) => {
   if(err) throw err;
-  jsonFile = JSON.parse(data);
   const userInput = process.argv[3];
   jsonFile.notes[jsonFile.nextId] = userInput;
   jsonFile.nextId++;
   const newData = JSON.stringify(jsonFile, null, 2);
-  fs.writeFile('./data.json', newData, 'utf8', ()=>console.log(jsonFile));
+  fs.writeFile('./data.json', newData, 'utf8', ()=>console.log(jsonFile.notes));
 });
 }
 
@@ -26,8 +25,18 @@ const del = () => {
   const indexToDelete = process.argv[3];
   delete jsonFile.notes[indexToDelete];
   const newData = JSON.stringify(jsonFile, null, 2);
-  fs.writeFile('./data.json', newData, 'utf8', () => console.log(jsonFile));
+  fs.writeFile('./data.json', newData, 'utf8', () => console.log(jsonFile.notes));
 });
+}
+
+const update = (index) => {
+  fs.readFile('./data.json', 'utf8', (err, data) => {
+    if (err) throw err;
+    const userInput = process.argv[4];
+    jsonFile.notes[index] = userInput;
+    const newData = JSON.stringify(jsonFile, null, 2);
+    fs.writeFile('./data.json', newData, 'utf8', () => console.log(jsonFile.notes));
+  });
 }
 
 switch(process.argv[2]) {
@@ -41,6 +50,13 @@ switch(process.argv[2]) {
 
   case 'delete':
     del();
+    break;
+
+  case 'update': {
+    index = process.argv[3]
+    update(index);
+  }
+
     break;
 
   default:
