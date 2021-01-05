950d2a43ada9f158efb0e8777b62e713db2a47a5
Josh Tjiauw <freezingmist2@gmail.com>
2021-01-05 15:19:44 -0800

    Added create and delete functions

--- node-notes-cli/app.js
+++ node-notes-cli/app.js
@@ -1,8 +1,48 @@
 const fs = require('fs');
-let jsonFile = {};
+let jsonFile = require('./data.json');
 
-const read = fs.readFile('data.json', 'utf8', (err, data) => {
+
+const read = () => {
+  fs.readFile('./data.json', 'utf8', (err, data) => {
+    if (err) throw err;
+    console.log(jsonFile.notes);
+  });
+}
+
+const create = () => {
+  fs.readFile('./data.json', 'utf8', (err, data) => {
   if(err) throw err;
   jsonFile = JSON.parse(data);
-  console.log(jsonFile.notes);
+  const userInput = process.argv[3];
+  jsonFile.notes[jsonFile.nextId] = userInput;
+  jsonFile.nextId++;
+  const newData = JSON.stringify(jsonFile, null, 2);
+  fs.writeFile('./data.json', newData, 'utf8', ()=>console.log(jsonFile));
 });
+}
+
+const del = () => {
+  fs.readFile('./data.json', 'utf8', (err, data) => {
+  const indexToDelete = process.argv[3];
+  delete jsonFile.notes[indexToDelete];
+  const newData = JSON.stringify(jsonFile, null, 2);
+  fs.writeFile('./data.json', newData, 'utf8', () => console.log(jsonFile));
+});
+}
+
+switch(process.argv[2]) {
+  case 'read':
+    read();
+    break;
+
+  case 'create':
+    create();
+    break;
+
+  case 'delete':
+    del();
+    break;
+
+  default:
+    console.log('Wrong Input');
+}
