bc037f446528508ce24840582a93c57b591ddc37
Josh Tjiauw <freezingmist2@gmail.com>
2021-01-06 15:19:23 -0800

    Finished express get JSON

---
+++ express-get-json/index.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const app = express();
+const grades = {
+  12: {
+    id: 12,
+    name: 'Tim Berners-Lee',
+    course: 'HTML',
+    score: 95
+  },
+  47: {
+    id: 47,
+    name: 'Brendan Eich',
+    course: 'JavaScript',
+    score: 100
+  },
+  273: {
+    id: 273,
+    name: 'Forbes Lindsay',
+    course: 'JavaScript',
+    score: 92
+  }
+};
+
+app.use(function (req, res) {
+  const arr = [];
+  for(let key in grades){
+    arr.push(grades[key])
+  }
+  res.json(arr);
+})
+
+app.listen(3000, () => {
+  console.log('Express server listening on port 3000');
+});
