21f3ccea14c412dfd04b15335e8de3fe21c972bd
Josh Tjiauw <freezingmist2@gmail.com>
2021-01-07 12:59:09 -0800

    Fixed the array situation in GET/POST

--- express-post-json/index.js
+++ express-post-json/index.js
@@ -2,26 +2,27 @@ const express = require('express');
 const app = express();
 let nextId = 1;
 const grades = {};
-const arr = [];
 
 app.use(express.json())
 
 app.get('/api/grades', (req, res) => {
+  const arr = [];
   for(const x in grades){
-    arr.push(grades);
+    arr.push(grades[x]);
   }
   res.json(arr);
 })
 
 app.post('/api/grades/', (req, res) => {
   const newGrade = {
-    id: arr.length + 1,
     name: req.body.name,
+    id: nextId,
     course: req.body.course,
-    score: req.body.score
+    score: +req.body.score
   }
-  arr.push(newGrade);
-  res.status(201).json(arr);
+  grades[nextId] = newGrade;
+  nextId++;
+  res.status(201).json(newGrade);
 })
 
 app.listen(3000, () => console.log('Listening on port 3000.'))
