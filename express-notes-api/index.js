ef676e2503b8a17fb93a59121610d9c6c89ec5f0
Josh Tjiauw <freezingmist2@gmail.com>
2021-01-07 18:00:24 -0800

    Finished Clients can DELETE a note by id.

--- express-notes-api/index.js
+++ express-notes-api/index.js
@@ -45,4 +45,23 @@ app.post('/api/notes', (req, res) => {
   }
 })
 
+
+app.delete('/api/notes/:id', (req, res) => {
+  const reqID = req.params.id;
+  id = parseInt(reqID);
+  if(id <= 0){
+    res.status(400).json({"error": "id must be a positive integer"});
+  }
+  else if(id > 0 && notes[id] === undefined){
+    res.status(404).json({"error": `cannot find note with id ${id}`})
+  }
+  else if(id > 0 && id in notes){
+    delete notes[id];
+    res.status(204).send(204);
+  }
+  else{
+    res.status(500).json({"error": "An unexpected error occurred."})
+  }
+})
+
 app.listen(3000, () => console.log('Listening to port 3000.'));
