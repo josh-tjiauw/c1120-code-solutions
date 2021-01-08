1d82b5cf13c5656bf06640bade31a46e3b7f7e66
Josh Tjiauw <freezingmist2@gmail.com>
2021-01-08 14:33:20 -0800

    Fixed the conditionals

--- express-notes-api/index.js
+++ express-notes-api/index.js
@@ -15,12 +15,13 @@ app.get('/api/notes', (req, res) => {
 })
 
 app.get('/api/notes/:id', (req, res) => {
-  const id = req.params.id;
-  if(id <= 0){
+  const getId = req.params.id;
+  const id = Number(getId);
+  if(!Number.isInteger(id) || id <= 0){
     const error400 = {error: "id must be a positive integer"}
     res.status(400).json(error400);
   }
-  else if(id in notes) {
+  else if(Number.isInteger(id) && id in notes) {
     res.json(notes[id]);
   }
   else{
@@ -44,7 +45,7 @@ app.post('/api/notes', (req, res) => {
   if(!req.body.content){
     res.status(400).json({ "error": "content is a required field"})
   }
-  else if (req.body.content){
+  else {
     const newNote = {
       id: data.nextId,
       content: req.body.content
@@ -53,9 +54,6 @@ app.post('/api/notes', (req, res) => {
     update(data.nextId, notes[data.nextId]);
     data.nextId++;
     res.status(201).json(newNote)
-  }
-  else{
-    res.status(500).json({"error": "An unexpected error occurred."})
   }
 })
 
@@ -66,10 +64,10 @@ app.delete('/api/notes/:id', (req, res) => {
     if (id <= 0) {
       res.status(400).json({ "error": "id must be a positive integer" });
     }
-    else if (id > 0 && notes[id] === undefined) {
+    else if (notes[id] === undefined) {
       res.status(404).json({ "error": `cannot find note with id ${id}` })
     }
-    else if (id > 0 && id in notes) {
+    else if (id in notes) {
       delete notes[id];
       update(id, notes[id])
       res.status(204).send(204);
@@ -90,10 +88,10 @@ app.put('/api/notes/:id', (req, res) => {
     else if (!req.body.content) {
       res.status(400).json({ "error": "content is a required field" })
     }
-    else if (id > 0 && notes[id] === undefined) {
+    else if (notes[id] === undefined) {
       res.status(404).json({ "error": `cannot find note with id ${id}` })
     }
-    else if (id > 0 && id in notes) {
+    else if (id in notes) {
       const newNote = req.body.content;
       notes[id].content = newNote;
       update(id, notes[id]);
