51dc927e6c393f1cd44d1c3df6ae0d448d77b328
Josh Tjiauw <freezingmist2@gmail.com>
2021-01-08 14:52:56 -0800

    w

--- javascript-typing-tutor/main.js
+++ javascript-typing-tutor/main.js
@@ -1,6 +1,7 @@
-var sentence = "grumpy wizards make toxic brew";
-var divContainer = document.querySelector('div.container');
-var h1Text = document.querySelector('.sentence');
+const sentence = "grumpy wizards make toxic brew";
+const divContainer = document.querySelector('div.container');
+const h1Text = document.querySelector('.sentence');
+
 
 function splitQuote(){
   sentence.split('').forEach(character => {
@@ -11,23 +12,19 @@ function splitQuote(){
 }
 splitQuote();
 
+
 document.addEventListener('keydown', (event)=>{
-  var spanArray = h1Text.querySelectorAll('span');
-  var correct = true;
-  var x = event.key;
-  spanArray.forEach((characterSpan, index)=>{
-    if (x == null) {
-      characterSpan.classList.add('black');
-      correct=false;
-    }
-    if (x === characterSpan.innerText) {
-      characterSpan.classList.add('green');
-      characterSpan.classList.remove('red');
+  const x = event.key;
+  const spanArray = h1Text.querySelectorAll('span');
+  let counter = 0;
+  while (counter < spanArray.length) {
+    if (spanArray[counter] === x) {
+      spanArray[counter].classList.add('green');
+      counter++
     }
     else {
-      characterSpan.classList.add('red');
-      characterSpan.classList.remove('green');
-      correct=false;
+      spanArray[counter].className.add('red');
     }
-  })
+  }
+
 })
