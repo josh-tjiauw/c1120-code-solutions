b866430a762dc300d47bda7f4c67f33616e1df98
Josh Tjiauw <freezingmist2@gmail.com>
2021-01-08 08:04:14 -0800

    Fixed the output on PUT to include the id.

--- express-notes-api/index.js
+++ express-notes-api/index.js
@@ -47,8 +47,7 @@ app.post('/api/notes', (req, res) => {
 
 
 app.delete('/api/notes/:id', (req, res) => {
-  const reqID = req.params.id;
-  id = parseInt(reqID);
+  const id = req.params.id;
   if(id <= 0){
     res.status(400).json({"error": "id must be a positive integer"});
   }
@@ -60,6 +59,27 @@ app.delete('/api/notes/:id', (req, res) => {
     res.status(204).send(204);
   }
   else{
+    res.status(500).json({"error": "An unexpected error occurred."})
+  }
+})
+
+app.put('/api/notes/:id', (req, res) => {
+  const id = req.params.id;
+  if (id <= 0){
+    res.status(400).json({ "error": "id must be a positive integer" })
+  }
+  else if(req.body.content.length == 0){
+    res.status(400).json({ "error": "content is a required field" })
+  }
+  else if(id > 0 && notes[id] === undefined){
+    res.status(404).json({ "error": `cannot find note with id ${id}`})
+  }
+  else if (id > 0 && id in notes){
+    const newNote = req.body.content;
+    notes[id].content = newNote;
+    res.status(200).json(notes[id])
+  }
+  else {
     res.status(500).json({"error": "An unexpected error occurred."})
   }
 })
