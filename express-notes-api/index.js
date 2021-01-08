603ca804e4c44a124adda5f198d5f4445d449bbf
Josh Tjiauw <freezingmist2@gmail.com>
2021-01-07 16:07:05 -0800

    Fixed the notes declaration to match with data.json

--- express-notes-api/index.js
+++ express-notes-api/index.js
@@ -1,23 +1,7 @@
 const express = require('express');
 const app = express();
-const notes = {
-  "1": {
-    "content": "The event loop is how a JavaScript runtime pushes asynchronous callbacks onto the stack once the stack is cleared.",
-    "id": 1
-  },
-  "2": {
-    "content": "Prototypal inheritance is how JavaScript objects delegate behavior.",
-    "id": 2
-  },
-  "3": {
-    "content": "In JavaScript, the value of `this` is determined when a function is called; not when it is defined.",
-    "id": 3
-  },
-  "4": {
-    "content": "A closure is formed when a function retains access to variables in its lexical scope.",
-    "id": 4
-  }
-};
+const data = require('./data.json')
+const notes = data.notes;
 
 app.get('/api/notes', (req, res) => {
   const arr = [];
