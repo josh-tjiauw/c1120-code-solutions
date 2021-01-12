0365d9a318605b2ff9ac913f77f728c622c9c3dd
Josh Tjiauw <freezingmist2@gmail.com>
2021-01-12 00:54:42 -0800

    Finished SQL Select

---
+++ sql-select/all-categories.sql
@@ -0,0 +1,3 @@
+--Get all columns of all rows in the categories table.--
+SELECT *
+FROM "categories"
---
+++ sql-select/five-customer-emails.sql
@@ -0,0 +1,4 @@
+--Get the customerId, firstName, lastName, and email of 5 customers.--
+SELECT "customerId", "firstName", "lastName", "email"
+FROM "customers"
+LIMIT 5;
