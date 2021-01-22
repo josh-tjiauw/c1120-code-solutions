b2efdc52aac484aed552b9b877eae30974c3b129
Josh Tjiauw <freezingmist2@gmail.com>
2021-01-22 12:43:33 -0800

    Fetch

---
+++ fetch/main.js
@@ -0,0 +1,8 @@
+fetch('https://jsonplaceholder.typicode.com/users')
+  .then(response => response.json())
+  .then(data => console.log(data));
+
+
+fetch('https://pokeapi.co/api/v2/pokemon/4')
+  .then(response => response.json())
+  .then(data => console.log(data))
