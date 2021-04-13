0f4bc3ee51714a4ab24ef98ba737f27e5778a9b6
Josh Tjiauw <freezingmist2@gmail.com>
2021-04-13 14:17:26 -0700

    finished common character count problem

--- algorithm-challenges/commonCharacterCount/index.js
+++ algorithm-challenges/commonCharacterCount/index.js
@@ -1,16 +1,27 @@
 function commonCharacterCount(s1, s2) {
   let numCommon = 0;
+  let char = null;
+  let char2 = null;
   for (let i = 0; i < s1.length; i++) {
-    if (s2.includes(s1[i])) {
-      numCommon++;
-      let index = s2.indexOf(s1[i])
-      console.log('index', index)
-      const news2 = s2.slice(index, s2.length);
-      console.log(news2)
+    if (char === null) {
+      if (s2.includes(s1[i])) {
+        numCommon++;
+        let index = s2.indexOf(s1[i])
+        char = s2.slice(0, index);
+        char2 = s2.slice(index + 1, s2.length)
+        char = char.concat(char2);
+      }
+    } else {
+      if (char.includes(s1[i])) {
+        numCommon++;
+        let index = char.indexOf(s1[i])
+        char2 = char.slice(index + 1, s2.length)
+        char = char.slice(0, index);
+        char = char.concat(char2);
+      }
     }
   }
-  console.log(numCommon)
   return numCommon;
 }
 
-commonCharacterCount("aabcc", "adcaa")
+commonCharacterCount("abca", "xyzbac")
