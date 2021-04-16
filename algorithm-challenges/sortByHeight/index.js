e144b3948bdf417d8e8a89e8382a62f47bf76e81
Josh Tjiauw <freezingmist2@gmail.com>
2021-04-16 15:01:09 -0700

    updated sortbyheight

--- algorithm-challenges/sortByHeight/index.js
+++ algorithm-challenges/sortByHeight/index.js
@@ -1,19 +1,46 @@
 function sortByHeight(a) {
   const newArr = [];
   const newArr2 = [];
+  const remainderArr = []
   const middle = a.length / 2;
+  let popped = null;
   for (let i = 0; i < middle; i++) {
-    if (a[i + 1] < middle) {
-      if (a[i] > a[i + 1]) {
-
+    if (a[i] > a[i + 1]) {
+      if (i === middle - 1) {
+        remainderArr.push(a[i])
       } else {
-        newArr.push(a[i]);
-        newArr2.push(a[middle + i])
+        popped = a[i];
       }
+    } else {
+      newArr.push(a[i]);
     }
   }
+
+  let count = 0;
+  for (let i = middle; i < a.length; i++) {
+    if (a[i] < popped && a[i] > newArr[newArr.length - 1] && count < 1) {
+      newArr.push(a[i])
+      remainderArr.map(index => {
+        newArr.push(index)
+      })
+      count++;
+    } else if (a[i] > popped) {
+      newArr2.push(popped)
+      popped = null;
+    } else if (popped && i === a.length - 1) {
+      newArr2.push(a[i])
+      newArr2.push(popped)
+    } else {
+      newArr2.push(a[i])
+    }
+  }
+
+  // newArr2.forEach(index => {
+  //   newArr.push(index)
+  // })
   console.log(newArr, newArr2)
   return newArr;
 }
 
-sortByHeight([-1, 150, 190, 170, -1, -1, 160, 180])
+sortByHeight([-1, -1, -1, -1, -1])
+//[-1, 150, 160, 170, -1, -1, 180, 190].
