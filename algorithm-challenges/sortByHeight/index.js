f9dc3161baf3361f6d711e42151cd445ae328297
Josh Tjiauw <freezingmist2@gmail.com>
2021-04-16 17:05:42 -0700

    finished sortByHeight problem

--- algorithm-challenges/sortByHeight/index.js
+++ algorithm-challenges/sortByHeight/index.js
@@ -1,46 +1,27 @@
 function sortByHeight(a) {
+  const treeArrayIndex = [];
+  const sortedArray = [];
   const newArr = [];
-  const newArr2 = [];
-  const remainderArr = []
-  const middle = a.length / 2;
-  let popped = null;
-  for (let i = 0; i < middle; i++) {
-    if (a[i] > a[i + 1]) {
-      if (i === middle - 1) {
-        remainderArr.push(a[i])
-      } else {
-        popped = a[i];
-      }
+  for (let i = 0; i < a.length; i++) {
+    if (a[i] === -1) {
+      treeArrayIndex.push(i)
     } else {
-      newArr.push(a[i]);
+      sortedArray.push(a[i])
     }
   }
 
-  let count = 0;
-  for (let i = middle; i < a.length; i++) {
-    if (a[i] < popped && a[i] > newArr[newArr.length - 1] && count < 1) {
-      newArr.push(a[i])
-      remainderArr.map(index => {
-        newArr.push(index)
-      })
-      count++;
-    } else if (a[i] > popped) {
-      newArr2.push(popped)
-      popped = null;
-    } else if (popped && i === a.length - 1) {
-      newArr2.push(a[i])
-      newArr2.push(popped)
+  sortedArray.sort((a, b) => {
+    return a - b;
+  })
+
+  for (let i = 0, sortedIndex = 0; i < a.length; i++) {
+    if (treeArrayIndex.includes(i)) {
+      newArr.push(-1);
     } else {
-      newArr2.push(a[i])
+      newArr.push(sortedArray[sortedIndex])
+      sortedIndex++;
     }
   }
-
-  // newArr2.forEach(index => {
-  //   newArr.push(index)
-  // })
-  console.log(newArr, newArr2)
   return newArr;
 }
-
-sortByHeight([-1, -1, -1, -1, -1])
-//[-1, 150, 160, 170, -1, -1, 180, 190].
+sortByHeight([23, 54, -1, 43, 1, -1, -1, 77, -1, -1, -1, 3])
