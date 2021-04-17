3579932abdb70cbb675d7c73461a9009eb24dcf8
Josh Tjiauw <freezingmist2@gmail.com>
2021-04-16 17:38:02 -0700

    revinpar

--- algorithm-challenges/reverseInParantheses/index.js
+++ algorithm-challenges/reverseInParantheses/index.js
@@ -1,3 +1,10 @@
 function reverseInParentheses(inputString) {
-
+  const split = inputString.split('')
+  console.log(split)
+  let tf = false;
+  const start = split.indexOf('(');
+  const end = split.indexOf(')')
+  console.log(split[start])
 }
+
+reverseInParentheses('foobar(baz)blim')
