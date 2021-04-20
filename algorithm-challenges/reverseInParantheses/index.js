55ee2aa65d0d2f69a802b7ca11a23a077fd1caf3
Josh Tjiauw <freezingmist2@gmail.com>
2021-04-19 17:16:35 -0700

    found a solution to revinparantheses

--- algorithm-challenges/reverseInParantheses/index.js
+++ algorithm-challenges/reverseInParantheses/index.js
@@ -1,10 +1,14 @@
 function reverseInParentheses(inputString) {
-  const split = inputString.split('')
-  console.log(split)
   let tf = false;
-  const start = split.indexOf('(');
-  const end = split.indexOf(')')
-  console.log(split[start])
+  const start = inputString.indexOf('(');
+  const end = inputString.indexOf(')')
+  const test = inputString.split('').splice(start + 1, end - start - 1).reverse().join('');
+  const test2 = inputString.split('').splice(end + 1).join('');
+  console.log(test2)
+  const newStr = inputString.split('').splice(0, start).join('').concat(test).concat(test2)
+  console.log(newStr);
+  console.log(test)
+  return newStr;
 }
 
 reverseInParentheses('foobar(baz)blim')
