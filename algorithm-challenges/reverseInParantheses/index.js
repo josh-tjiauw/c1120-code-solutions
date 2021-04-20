b5199791b2f14b519608f0b7f34b64fa9fe898f9
Josh Tjiauw <freezingmist2@gmail.com>
2021-04-20 10:00:15 -0700

    finished 7/7

--- algorithm-challenges/reverseInParantheses/index.js
+++ algorithm-challenges/reverseInParantheses/index.js
@@ -1,26 +1,47 @@
 function reverseInParentheses(inputString) {
-  let tf = false;
-  const regex = /[(]/g;
-  var count = inputString.match(regex).length
-  console.log(count)
+  let outputString = inputString;
+  let nested = false;
+  if (outputString.includes('(')) {
+    const outer = outputString.indexOf('(')
+    const checker = outputString.split('').splice(outer + 1, outputString.length);
+    if (checker.includes('(') && checker.indexOf('(') < checker.indexOf(')')) {
+      nested = true;
+    }
+  }
   const rev = (input) => {
     const start = input.indexOf('(');
     const end = input.indexOf(')')
     const test = input.split('').splice(start + 1, end - start - 1).reverse().join('');
     const test2 = input.split('').splice(end + 1).join('');
-    const newStr = input.split('').splice(0, start).join('').concat(test).concat(test2);
     return input.split('').splice(0, start).join('').concat(test).concat(test2);
   }
-  let newStr = rev(inputString)
+
   let i = 0;
   while (i !== -1) {
-    if (newStr.includes('(')) {
-      newStr = rev(newStr);
+    if (i === 0 && !nested) {
+      outputString = rev(outputString);
+      if (outputString.includes('(')) {
+        i = 0;
+      } else {
+        i = -1;
+      }
+    } else if (i === 0 && nested) {
+      const firstPar = inputString.indexOf('(')
+      const secondPar = inputString.indexOf('(', firstPar + 1)
+      const secondClosePar = inputString.indexOf(')')
+      const firstClosePar = inputString.indexOf(')', secondClosePar + 1)
+      const innerReverse = inputString.split('').splice(secondPar + 1, secondClosePar - secondPar - 1).reverse();
+      let outerReverse = inputString.split('').splice(firstPar + 1, secondPar - firstPar - 1);
+      outerReverse = outerReverse.concat(innerReverse).reverse();
+      const lastChar = inputString.split('').splice(firstClosePar + 1)
+      outputString = inputString.split('').splice(0, firstPar).concat(outerReverse).concat(lastChar).join('')
+      i = -1;
     } else {
       i = -1;
-      return newStr;
     }
   }
+  return outputString;
 }
 
-reverseInParentheses('foo(bar(baz))blim')
+const hahaxd = reverseInParentheses('foo(barbaz)blim')
+console.log(hahaxd)
