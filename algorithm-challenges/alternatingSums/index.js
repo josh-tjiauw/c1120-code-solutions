e2a89d5998223826f3b0cffba39e414c5365fa52
Josh Tjiauw <freezingmist2@gmail.com>
2021-04-20 11:01:23 -0700

    finished alternating sums

---
+++ algorithm-challenges/alternatingSums/index.js
@@ -0,0 +1,18 @@
+function alternatingSums(a) {
+  const arrOfWeights = [];
+  let weightT1 = 0;
+  let weightT2 = 0;
+  for (let i = 0; i < a.length; i++) {
+    if (i % 2 === 0) {
+      weightT1 += a[i];
+    } else {
+      weightT2 += a[i];
+    }
+  }
+  arrOfWeights.push(weightT1);
+  arrOfWeights.push(weightT2);
+  return arrOfWeights;
+}
+
+const test = alternatingSums([50, 60, 60, 45, 70])
+console.log(test);
