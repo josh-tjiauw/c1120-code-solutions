645dcdc398ca63d44add6809d2f5ea45f82148c5
Josh Tjiauw <freezingmist2@gmail.com>
2021-04-26 16:22:49 -0700

    finished palindromerearranging with regex

--- algorithm-challenges/palindromeRearranging/index.js
+++ algorithm-challenges/palindromeRearranging/index.js
@@ -1,24 +1,5 @@
 function palindromeRearranging(inputString) {
-  let tf = false;
-  const palindromeChecker = str => {
-    let tf2 = true;
-    for (let i = 0; i < str.length; i++) {
-      if (str[i] !== str[str.length - i - 1]) {
-        tf2 = false;
-        return tf2;
-      }
-    }
-    return tf2
-  }
-
-  if (palindromeChecker(inputString)) {
-    tf = true;
-    console.log(tf)
-  } else {
-    console.log('asdf')
-    console.log((inputString.match(/[^a]/g) || []).length)
-  }
-  return tf;
+  return inputString.split('').sort().join('').replace(/(\w)\1/g, '').length < 2;
 }
 
-palindromeRearranging("abbccaccbba")
+console.log(palindromeRearranging("aabbccaccbb"))
