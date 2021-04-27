c6ef3d3f6392173e574b07be8a8de2d4ffc0e496
Josh Tjiauw <freezingmist2@gmail.com>
2021-04-27 10:55:56 -0700

    cleaned up code

--- algorithm-challenges/isIPv4Address/index.js
+++ algorithm-challenges/isIPv4Address/index.js
@@ -1,21 +1,8 @@
 function isIPv4Address(inputString) {
   const split = inputString.split('.')
-  if (split.length !== 4) {
-    return false
-  }
-
-  const checkValidIP = split.every(index => {
-    if (index.length > 1 && index[0] === '0') {
-      return false
-    }
-
-    if (index === '' || index > 255 || isNaN(index)) {
-      return false
-    }
-
-    return true
+  return split.length === 4 && split.every(index => {
+    return index !== '' && !isNaN(index) && index >= 0 && index <= 255
   })
-  return checkValidIP
 }
 
-console.log(isIPv4Address("64.233.161.1a"))
+console.log(isIPv4Address("64.233.161.1"))
