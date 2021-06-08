4fb20ed42f918512c0f32d61f7c1d9a7ad3f0d43
Josh Tjiauw <freezingmist2@gmail.com>
2021-06-07 18:27:49 -0700

    Finished box blur problem

--- algorithm-challenges/box-blur/index.js
+++ algorithm-challenges/box-blur/index.js
@@ -1,23 +1,20 @@
-function boxBlur(image) {
-  const newArray = [];
-  for (let height = 0, width = 0; height < image.length - 2; height++) {
-    if (image[0].length === 3) {
-      const splice = Math.floor((image[height].splice(width, 3).reduce((total, index) => index + total, 0) +
-        image[height + 1].splice(width, 3).reduce((total, index) => index + total, 0) +
-        image[height + 2].splice(width, 3).reduce((total, index) => index + total, 0)) / 9)
-      newArray.push([splice]);
-      return newArray;
-    }
+boxBlur = I => {
+  //Slice off the border
+  var B = I.slice(1, -1).map(e => e.slice(1, -1))
 
-    const splice = Math.floor((image[height].splice(width, 3).reduce((total, index) => index + total, 0) +
-      image[height + 1].splice(width, 3).reduce((total, index) => index + total, 0) +
-      image[height + 2].splice(width, 3).reduce((total, index) => index + total, 0)) / 9)
-    console.log(splice)
-    newArray.push([splice]);
-    console.log(newArray)
-  }
-  return newArray
+  //Replace every element with the average
+  // of its neighbors
+  return B.map((row, i) =>
+    row.map((elem, j) => {
+      var sum = 0
+      for (var x = 0; x < 3; ++x)
+        for (var y = 0; y < 3; ++y)
+          sum += I[i + x][j + y]
+      return sum / 9 | 0
+    })
+  )
 }
+
 
 boxBlur(
   [[36, 0, 18, 9],
